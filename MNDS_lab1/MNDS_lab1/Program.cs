efe0828e5a1aeb0e0114d919d4d9a863b533a2d5
Spekles <47836294+Spekles@users.noreply.github.com>
2023-01-27 08:54:56 +0200

    Add files via upload

---
+++ MNDS_lab1/MNDS_lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MNDS_lab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Створити об‘єкт-деталь Ring:
+            Class1 Ring = new Class1();
+            //На об‘єкті Ring виконати функцію Elasticity():
+            Ring.Elasticity();
+            //На об‘єкті Ring виконати функцію Elasticity_FEM():
+            Ring.Elasticity_FEM();
+        }
+    }
+}
